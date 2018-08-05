5bb865cad8a2db011e0ec6a152ad4e62840fc600
Sheldon Ketterer <smketterer@gmail.com>
2018-08-05 14:19:54 -0700

    Rewrite server to use socketserver, so less work is required for keeping server alive, etc.

--- client.py
+++ client.py
@@ -5,10 +5,10 @@ log = logging.getLogger('client')
 FORMAT_CONS = '%(asctime)s %(name)-12s %(levelname)-5s %(message)s'
 logging.basicConfig(level=logging.DEBUG,format=FORMAT_CONS)
 
-def send_datagram(host='127.0.0.1', port=1234):
-    MESSAGE = "Hello, World!".encode() # Need binary representation, alternatively use struct.unpack(fmt,str)
+def send_test_datagram(host='127.0.0.1', port=1234):
+    MESSAGE = "Hello, World!".encode() # Need binary representation, alternatively use struct.unpack(fmt,str) in the server
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, UDP settings
     s.sendto(MESSAGE, (host, port))
     log.debug("Sent: %s" % (MESSAGE))
 
-send_datagram()
+send_test_datagram()
--- udp_server.py
+++ udp_server.py
@@ -1,20 +1,21 @@
 import logging
-import socket
+import socketserver
 
 log = logging.getLogger('udp_server')
-
-def udp_server(host='127.0.0.1', port=1234):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    log.info("Listening on udp %s:%s" % (host, port))
-    s.bind((host, port))
-    while True:
-        (data, addr) = s.recvfrom(128*1024)
-        yield data
-
 FORMAT_CONS = '%(asctime)s %(name)-12s %(levelname)-5s %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=FORMAT_CONS)
 
-for data in udp_server():
-    log.debug("%r" % (data,))
+class UDPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        data = self.request[0].strip()
+        socket = self.request[1]
+        log.debug("%s: %r" % (self.client_address[0],data))
+        socket.sendto(data.upper(), self.client_address)
+
+def udp_server(host='127.0.0.1', port=1234):
+    with socketserver.UDPServer((host, port), UDPHandler) as server:
+        log.info("Listening on UDP %s:%s" % (host, port))
+        log.info("Press CTRL-C to shut down server.")
+        server.serve_forever()
+
+udp_server()
