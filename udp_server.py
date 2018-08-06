26fe6c9fbeeae2a9d5d16c850b92c507cd87da1f
Sheldon Ketterer <smketterer@gmail.com>
2018-08-05 17:44:35 -0700

    Try decoding the data. Need to figure out how to strip out unicode characters.

--- udp_server.py
+++ udp_server.py
@@ -1,16 +1,27 @@
 import logging
 import socketserver
+import struct
 
 log = logging.getLogger('udp_server')
 FORMAT_CONS = '%(asctime)s %(name)-12s %(levelname)-5s %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=FORMAT_CONS)
 
 class UDPHandler(socketserver.BaseRequestHandler):
+    def setup(self):
+        """ Called before the handle() method to perform any initialization actions required. """
+        pass
+
     def handle(self):
-        data = self.request[0].strip()
+        data = self.request[0]
         socket = self.request[1]
-        log.debug("%s: %r" % (self.client_address[0],data))
+        decoded_data = data
+        # decoded_data = data.decode('utf-8','ignore')
+        log.debug("%s: %r" % (self.client_address[0], decoded_data))
         socket.sendto(data.upper(), self.client_address)
+
+    def finish(self):
+        """ Called after the handle() method to perform any clean-up actions required. """
+        pass
 
 def udp_server(host='127.0.0.1', port=1234):
     with socketserver.UDPServer((host, port), UDPHandler) as server:
